5647e1fa309751804612b3253055aa6e8c7c0321
akshi0409 <akshita.asah@gmail.com>
2023-12-08 21:40:01 +0530

    Refactored : Added Uc4 to match mail

--- UserRegistration/src/main/java/UserRegistrationProblem.java
+++ UserRegistration/src/main/java/UserRegistrationProblem.java
@@ -9,6 +9,11 @@ public class UserRegistrationProblem {
         return (Pattern.matches("^[A-Za-z0-9]+"+"([+._-][A-za-z0-9]+)?"+"@[A-Za-z0-9]+"+"[.]([A-za-z0-9])+"+"([.][A-Za-z0-9]{0,2})?",mail));
     }
 
+    public boolean matchNumber(String number)
+    {
+        return (Pattern.matches("^[0-9]{1,2}\\s[1-9][0-9]{9}",number));
+    }
+    
     public static void main(String[] args) {
         UserRegistrationProblem user = new UserRegistrationProblem();
         System.out.println(user.matchName("Akshita"));
