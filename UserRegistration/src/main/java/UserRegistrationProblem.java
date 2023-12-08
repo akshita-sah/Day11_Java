06613112ee6fe7ae3e5b8107f02e608aa66f5746
akshi0409 <akshita.asah@gmail.com>
2023-12-08 21:38:25 +0530

    Refactored : Added UC 2 to check last name

--- UserRegistration/src/main/java/UserRegistrationProblem.java
+++ UserRegistration/src/main/java/UserRegistrationProblem.java
@@ -8,11 +8,11 @@ public class UserRegistrationProblem {
     public boolean matchMail(String mail){
         return (Pattern.matches("^[A-Za-z0-9]+"+"([+._-][A-za-z0-9]+)?"+"@[A-Za-z0-9]+"+"[.]([A-za-z0-9])+"+"([.][A-Za-z0-9]{0,2})?",mail));
     }
-    
+
     public static void main(String[] args) {
         UserRegistrationProblem user = new UserRegistrationProblem();
         System.out.println(user.matchName("Akshita"));
-        System.out.println(user.matchName("Aks"));
+        System.out.println(user.matchName("Sah"));
         System.out.println(user.matchMail("abcxyz@bl.co.co.in"));
         System.out.println(user.matchNumber("09046371890"));
         System.out.println(user.matchPassword("wUf@9fabcdeeee"));
